7e4347af5c7943f5cd6705730808390520a8fea3
sotrosh <sotrosh@d3630bda-fbf1-ef55-2202-671b3b89648c>
2011-11-11 15:17:07 +0000

    Added project skeleton.

---
+++ app/app/SVGViewController.h
@@ -0,0 +1,13 @@
+//
+//  SVGViewController.h
+//  app
+//
+//  Created by sotrosh on 11/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVGViewController : UIViewController
+
+@end
