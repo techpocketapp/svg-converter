5241fe3dfaae00258e1fa033881dac5b8c012cbf
sotrosh <sotrosh@d3630bda-fbf1-ef55-2202-671b3b89648c>
2011-11-11 16:09:53 +0000

    Updated interfaces.

---
+++ trunk/framework/SVG.h
@@ -0,0 +1,16 @@
+//
+//  SVG.h
+//  app
+//
+//  Created by sotrosh on 11/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SVG : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, assign) float scale;
+
+@end
